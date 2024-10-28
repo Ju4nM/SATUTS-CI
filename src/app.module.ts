4fb612fbe169039acb43c56b60134c2fb9f29c1f
Ju4nM <jenriquemflores@gmail.com>
2024-10-28 13:13:05 -0700

    Configuración de TypeORM, ConfigModule, ValidationPipes y habilitación de CORS agregado

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: ".env"
+    }),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: ["dist/**/*.entity.js"],
+      synchronize: process.env.APP_ENVIRONMENT === "development",
+      ssl: process.env.APP_ENVIRONMENT !== "development"
+    })
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
